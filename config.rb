d44ca4a7325b2d0987ed76c67756b41edacda49c
Dominick Washburn <a041697@Harlands-MacBook-Pro.local>
2014-10-20 15:19:15 -0500

    initial commit: files pulled from Dominick Washburn's (domwashburn.com) personal repository and modified

---
+++ config.rb
@@ -0,0 +1,21 @@
+# Require any additional compass plugins here.
+require 'breakpoint'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/scss"
+images_dir = "assets/images"
+javascripts_dir = "assets/scripts"
+fonts_dir = "assets/fonts"
+
+output_style = :expanded
+environment = :production
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+preferred_syntax = :sass
